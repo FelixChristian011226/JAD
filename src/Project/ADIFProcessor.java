59d92b545834b2e0f6f5c1fee139c58dcc2f4bb0
FelixChristian011226 <1930552052@qq.com>
2023-11-11 20:13:11 +0800

    UPDATE. ADIFProcessor version 2.1 - 250/250 on PTA.

--- src/Project/ADIFProcessor.java
+++ src/Project/ADIFProcessor.java
@@ -18,22 +18,14 @@ public class ADIFProcessor {
         StringBuilder currentRecord = new StringBuilder();
 
         while ((line = reader.readLine()) != null) {
-            if (line.contains("<EOR>")) {
-                currentRecord.append(line, 0, line.indexOf("<EOR>")).append("\n");
+            if (line.toUpperCase().contains("<EOR>")) {
+                currentRecord.append(line, 0, line.toUpperCase().indexOf("<EOR>")).append("\n");
                 processRecord(currentRecord.toString(), fields, records);
                 currentRecord.setLength(0);
-                currentRecord.append(line.substring(line.indexOf("<EOR>") + 5)).append("\n");
-            } else if (line.contains("<eor>")) {
-                currentRecord.append(line, 0, line.indexOf("<eor>")).append("\n");
-                processRecord(currentRecord.toString(), fields, records);
+                currentRecord.append(line.substring(line.toUpperCase().indexOf("<EOR>") + 5)).append("\n");
+            } else if (line.toUpperCase().contains("<EOH>")) {
                 currentRecord.setLength(0);
-                currentRecord.append(line.substring(line.indexOf("<eor>") + 5)).append("\n");
-            }  else if (line.contains("<EOH>")) {
-                currentRecord.setLength(0);
-                currentRecord.append(line.substring(line.indexOf("<EOH>") + 5)).append("\n");
-            } else if (line.contains("<eoh>")) {
-                currentRecord.setLength(0);
-                currentRecord.append(line.substring(line.indexOf("<eoh>") + 5)).append("\n");
+                currentRecord.append(line.substring(line.toUpperCase().indexOf("<EOH>") + 5)).append("\n");
             } else {
                 currentRecord.append(line).append("\n");
             }
@@ -99,17 +91,18 @@ public class ADIFProcessor {
         int index = 0;
 
         Map<String, String> recordMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-        Pattern pattern = Pattern.compile("<([^>]+):(\\d+)>");
+        Pattern pattern = Pattern.compile("<([^>\\u4E00-\\u9FA5]+):(\\d+)>");
 
         for (String line : lines) {
             Matcher matcher = pattern.matcher(line);
             index = 0;
-            while (matcher.find(index)) {
+            while (index<line.length() && matcher.find(index)) {
                 String fieldName = matcher.group(1).toUpperCase(); // Convert to uppercase
                 int fieldLength = Integer.parseInt(matcher.group(2));
-                String fieldValue = line.substring(matcher.end(), matcher.end() + fieldLength);
-                index = matcher.end() + fieldLength;
-                recordMap.put(fieldName, fieldValue.substring(0, fieldLength));
+                String original_fieldValue = line.substring(matcher.end(), Math.min((matcher.end() + fieldLength), line.length()));
+                String fieldValue = getFieldValue(original_fieldValue, fieldLength);
+                index = matcher.end() + Math.min(fieldValue.length(), fieldLength);
+                recordMap.put(fieldName, fieldValue.substring(0, Math.min(fieldValue.length(), fieldLength)));
                 fields.put(fieldName, 1);
             }
         }
@@ -124,4 +117,35 @@ public class ADIFProcessor {
         // Store the record in the records map
         records.put(recordMap.get("QSO_DATE") + recordMap.get("TIME_ON"), recordMap);
     }
+
+    private static String getFieldValue(String original, int fieldLength) {
+        String fieldValue = "";
+        while(fieldLength>0) {
+            char c = original.charAt(0);
+            if(isChineseCharacter(c)) {
+                if(fieldLength==1) {
+                    break;
+                }
+                fieldValue += c;
+                fieldLength -= 2;
+            } else {
+                fieldValue += c;
+                fieldLength -= 1;
+            }
+            original = original.substring(1);
+        }
+        return fieldValue;
+    }
+
+    private static boolean isChineseCharacter(char ch) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(ch);
+        return ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION;
+    }
+
 }
